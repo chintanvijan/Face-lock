c6eeeb599622ca696396687a1d57fdab6724dc24
Chintan Vijan <35058356+chintanvijan@users.noreply.github.com>
2018-12-10 22:29:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+def getfile():
+	Tk().withdraw() 
+	filename = askopenfilename() 
+	return filename
+
+def getuserimage():
+	Tk().withdraw() 
+	filename = askopenfilename() 
+	fi = open("facepath.txt","w")
+	fi.write(filename)
+	fi.close()
+
+def hide(t):
+	c="attrib +h +s +r /s /d "+t
+	r=os.system(c)
+	nli = t.split(".")
+	s,k="",""
+	#nli[-2]=nli[-1]+"-"+nli[-2]
+	for i in range(len(nli)-1):
+		s+=nli[i]
+	fmain = open("code.py")
+	fr = fmain.read()
+	fmain.close()
+	fname=s+"-"+nli[-1]+".py"
+	fi = open(fname,"w")
+	fi.write(fr)
+	fi.close()
+	f = open("facepath.txt")
+	fr1 = f.read()
+	f.close()
+	nli1=fname.split("\\")
+	for i in range(len(nli1)-1):
+		k+=nli1[i] 
+	ts=k+"facepath.txt"
+	print(ts)
+	f1 = open(ts,"w")
+	f1.write(fr1)
+	f1.close()
+	cm = "attrib +h +s +r /s /d "+ts
+	os.system(cm)
+
+
+
+
